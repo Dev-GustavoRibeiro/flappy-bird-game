5b7ce6afb6dd48e54fa400a42eef71d624b36de5
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2020-02-26 23:33:05 -0300

    Implementing the point counting logic and creating the main function

--- js/flappy.js
+++ js/flappy.js
@@ -99,13 +99,47 @@ function Bird(heightGame) {
     this.setY(heightGame / 2)
 }
 
-const barriers = new MoveBarriers(700, 1200, 200, 400)
-const bird = new Bird(700)
-const gameArea = document.querySelector('[flappy]')
+// const barriers = new MoveBarriers(700, 1200, 200, 400)
+// const bird = new Bird(700)
+// const gameArea = document.querySelector('[flappy]')
 
-gameArea.appendChild(bird.element)
-barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
-setInterval(() => {
-    barriers.animate()
-    bird.animate()
-}, 20);
+// gameArea.appendChild(bird.element)
+// barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
+// setInterval(() => {
+//     barriers.animate()
+//     bird.animate()
+// }, 20);
+
+function Progress() {
+    this.element = newElement('span', 'progress')
+    this.updatePoints = points => {
+        this.element.innerHTML = points
+    }
+    this.updatePoints(0)
+}
+
+function FlappyBird() {
+    let points = 0
+
+    const gameArea = document.querySelector('[flappy]')
+    const height = gameArea.clientHeight
+    const width = gameArea.clientWidth
+
+    const progress = new Progress()
+    const barriers = new MoveBarriers(height, width, 200, 400, 
+        () => progress.updatePoints(++points))
+    const bird = new Bird(height)
+
+    gameArea.appendChild(progress.element)
+    gameArea.appendChild(bird.element)
+    barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
+
+    this.start = () => {
+        const timer = setInterval(() => {
+           barriers.animate()
+           bird.animate() 
+        }, 20);
+    }
+}
+
+new FlappyBird().start()
